63dfb2602972ad9ab7026f5ea7525b5808ddc3ed
AngeloRafaelbr <angeloprofissional@gmail.com>
2026-04-06 00:13:22 -0300

    Todos /hooks alterados para puxar email do cookie/JWT, para maior segurança - corrigido importação faltante

--- src/pages/api/transaction/transactionDelete.js
+++ src/pages/api/transaction/transactionDelete.js
@@ -1,6 +1,7 @@
 // src/pages/api/transaction/transactionDelete.js
 
 import { PrismaClient } from '@prisma/client';
+import { requireAuth } from '@/middleware/auth';
 
 const prisma = new PrismaClient();
 
